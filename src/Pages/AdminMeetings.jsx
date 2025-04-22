f1daf041b16d34e45d31d9d958f36bbf3eb38925
12yasaswi56 <yasaswikopparapu624@gmail.com>
2025-04-22 21:36:37 +0530

    changes made in admin meetings file

--- src/Pages/AdminMeetings.jsx
+++ src/Pages/AdminMeetings.jsx
@@ -181,7 +181,6 @@
 // export default AdminMeetings;
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Footer from "../components/Footer";
 import "../pagesCSS/AdminMeeting.css";
 
 // Custom sanitization function to prevent XSS
@@ -253,7 +252,6 @@ const AdminMeetings = () => {
           })}
         </ul>
       )}
-      <Footer />
     </div>
   );
 };
