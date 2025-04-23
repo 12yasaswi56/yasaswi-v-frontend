eaa76e32a9d3461a112ed4dd93c6cd97947da25f
12yasaswi56 <yasaswikopparapu624@gmail.com>
2025-04-23 11:19:43 +0530

    FC

--- src/Pages/OTPVerification.jsx
+++ src/Pages/OTPVerification.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"; // Redirect after OTP verification
-import "../pagesCSS/OTPVerification.css"
+import "../pagesCSS/OtpVerification.css"
 const OTPVerification = () => {
     const [otp, setOtp] = useState("");
     const [message, setMessage] = useState("");
